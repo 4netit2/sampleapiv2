9447c28b0307e1bd135afad6db641b53d1edc766
4netIT <21337066+4netit2@users.noreply.github.com>
2022-06-16 15:45:38 +0300

    Initial commit

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,31 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var persons = new string[]{
+                "Mohamad",
+                "Donald duck",
+                "Mickey",
+                "Batman",
+                "Thor",
+                "Tony Stark",
+                "Mercedes",
+                "BMW",
+                "Jeep",
+                "Honda",
+                "Ferrari", 
+                "Lambo",
+                "Renault"
+            };
+           return Ok(persons);
+        }
+    }
+}
